a0ea15c6b43eb026210f20167d2d24dc8db43f22
Hyeon-Jun-Kim <59905688+Hyeon-Jun-Kim@users.noreply.github.com>
2021-04-17 15:15:54 +0900

    first commit

---
+++ app/src/main/java/com/hanshin/ncs_imprintsaga/MainActivity.java
@@ -0,0 +1,64 @@
+package com.hanshin.ncs_imprintsaga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button main_MY, main_SHOP, main_SETTING, main_TRAINING;
+    Button stageBtn[] = new Button[9];
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        main_MY = findViewById(R.id.main_my_btn);
+        main_SHOP = findViewById(R.id.main_shop_btn);
+        main_SETTING = findViewById(R.id.main_setting_btn);
+        main_TRAINING = findViewById(R.id.main_training_btn);
+
+        main_MY.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        main_SHOP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        main_SETTING.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        main_TRAINING.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        for(int i =0;i<9;i++){
+            int k = getResources().getIdentifier("main_stage1_"+(i+1), "id", getPackageName());
+            stageBtn[i] = findViewById( k );
+        }
+
+        stageBtn[0].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mapSelectDialog mapSelectDialog = new mapSelectDialog(MainActivity.this);
+                mapSelectDialog.callFunction();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/hanshin/ncs_imprintsaga/mapSelectDialog.java
@@ -0,0 +1,47 @@
+package com.hanshin.ncs_imprintsaga;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.media.Rating;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class mapSelectDialog {
+    private Context context;
+
+    public mapSelectDialog(Context context) {
+        this.context = context;
+    }
+
+    public void callFunction() {
+
+        // 커스텀 다이얼로그를 정의하기위해 Dialog클래스를 생성한다.
+        final Dialog dlg = new Dialog(context);
+
+        // 액티비티의 타이틀바를 숨긴다.
+        dlg.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // 커스텀 다이얼로그의 레이아웃을 설정한다.
+        dlg.setContentView(R.layout.stage_info);
+
+        // 커스텀 다이얼로그를 노출한다.
+        dlg.show();
+
+        // 커스텀 다이얼로그의 각 위젯들을 정의한다.
+        final TextView stageTv = (TextView) dlg.findViewById(R.id.stageTv);
+        final RatingBar rankRb = (RatingBar) dlg.findViewById(R.id.rankRb);
+        final TextView levelTv = (TextView) dlg.findViewById(R.id.levelTb);
+        final TextView rateTv = (TextView) dlg.findViewById(R.id.rateTv);
+        final Button selectBtn = (Button) dlg.findViewById(R.id.selectBtn);
+
+        selectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dlg.dismiss();
+            }
+        });
+    }
+}
