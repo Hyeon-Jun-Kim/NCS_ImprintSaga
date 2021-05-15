daa0b2757f7820e47d626cae4fafbd11bbbc2ae4
Hyeon-Jun-Kim <59905688+Hyeon-Jun-Kim@users.noreply.github.com>
2021-05-15 17:04:50 +0900

    battle page timer

--- app/src/main/java/com/hanshin/ncs_imprintsaga/BattleActivity.java
+++ app/src/main/java/com/hanshin/ncs_imprintsaga/BattleActivity.java
@@ -44,17 +44,17 @@ public class BattleActivity extends AppCompatActivity {
     CsvData[] dataArray = new CsvData[40];
     TextView questionTV, timerTV, answerTV, hintTV;
     ProgressBar userPB, enemyPB, timerPB;
-    Button next;
     Handler handler;
-    int count = 1;
-    int countTime = 10;
-    CountDownTimer countDownTimer;
+    int count_word = 1;
     LinearLayout setBtnLayout;
     String word;
     int countWordLength = 0;
     Button wordBtn[] = new Button[10];
 
-    TextView test;
+    int count_timer= 11;
+    CountDownTimer countDownTimer_battle;
+    int count_onStart= 3;
+    CountDownTimer countDownTimer_onStart;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -68,8 +68,6 @@ public class BattleActivity extends AppCompatActivity {
         userPB = findViewById(R.id.userPB);
         enemyPB = findViewById(R.id.enemyPB);
         timerPB = findViewById(R.id.timerPB);
-
-        next = findViewById(R.id.testbtn);
         setBtnLayout = findViewById(R.id.wordBtns);
 
         InputStream input = getResources().openRawResource(R.raw.words);
@@ -79,18 +77,53 @@ public class BattleActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                count++;
-                questionTV.setText(dataArray[count].meaning);
+        countDownTimer_onStart();
+        countDownTimer_onStart.start();
+    }
+
+    public void readDataFromCsv(InputStream input) throws IOException {
+
+        InputStreamReader streamReader = null;
+        streamReader = new InputStreamReader(input, Charset.forName("UTF-8"));
+        CSVReader reader = new CSVReader(streamReader); // 1. CSVReader 생성
+
+        String[] nextLine;
+
+        int j = 0;
+
+        while ((nextLine = reader.readNext()) != null) {   // 2. CSV 파일을 한줄씩 읽음
+            Log.d("TAG", nextLine[0] + "|" + nextLine[1] + "|"
+                    + nextLine[2] + "|" + nextLine[3] + "\n");
+            dataArray[j] = new CsvData();
+            dataArray[j].number = nextLine[0];
+            dataArray[j].words = nextLine[1];
+            dataArray[j].meaning = nextLine[2];
+            dataArray[j].grade = nextLine[3];
+            j++;
+            if (j == 30)
+                break;
+        }
+    }
+
+    public void countDownTimer_onStart(){
+
+        countDownTimer_onStart = new CountDownTimer(3000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                hintTV.setText(String.valueOf(count_onStart));
+                count_onStart --;
+            }
+            public void onFinish() {
+                hintTV.setText(String.valueOf("Start!"));
+                countDownTimer_onStart.cancel();
+                count_word++;
+                questionTV.setText(dataArray[count_word].meaning);
 
                 setBtnLayout.removeAllViews(); // 버튼 레이아웃 초기화
                 answerTV.setText(""); // 정답창 초기화
                 hintTV.setText(""); // 힌트창 초기화
                 countWordLength=0;
 
-                word = dataArray[count].words; // CSC 파일에서 단어를 불러옴
+                word = dataArray[count_word].words; // CSC 파일에서 단어를 불러옴
                 String splitWord[] = word.split(""); // 단어를 한글자씩 나누어 배열화
                 splitWord = shuffle(splitWord); // 배열을 랜덤으로 섞는다
 
@@ -120,85 +153,86 @@ public class BattleActivity extends AppCompatActivity {
                                 if(word.equals(answerTV.getText())) {
                                     hintTV.setText("HIT!");
                                     enemyPB.setProgress(enemyPB.getProgress() - 1);
-                                    count++;
+                                    count_word++;
                                 } else {
                                     hintTV.setText("! "+word+" !");
                                     userPB.setProgress(userPB.getProgress() - 1);
-                                    count++;
+                                    count_word++;
                                 }
                             }
                         }
                     });
                     setBtnLayout.addView(wordBtn[i]); // 동적 버튼 추가
                 }
-
-//        count = 0;
-//        //while (userPB.getProgress() == 0 || enemyPB.getProgress() == 0) {};
-//        countTime = 10;
-//        countDownTimer();
-//        countDownTimer.start();
-//        while (countTime > -1) {
-//            questionTV.setText(dataArray[count].meaning);
-//            word = dataArray[count].words;
-//            String[] splitWord = word.split("");
-//            for (int i = 0; i < splitWord.length; i++) {
-//                wordBtn[i].setHeight(20);
-//                wordBtn[i].setWidth(20);
-//                wordBtn[i].setText(splitWord[i]);
-//                setBtnLayout.addView(wordBtn[i]);
-//            }
-//            //answerTV.setText(word[0]);
-//        }
-
-        ////////////////////////////////////////////////////////////////////////////
-
-
-//                for(int i=0;i<splitWords.length;i++) {
-//                    test.setText(splitWords[2]);
-//                }
-//                countTime = 10;
-//                countDownTimer();
-//                countDownTimer.start();
+                countDownTimer_battle();
+                countDownTimer_battle.start();
             }
-        });
-
+        };
     }
 
-    public void readDataFromCsv(InputStream input) throws IOException {
+    public void countDownTimer_battle() {
+        countDownTimer_battle = new CountDownTimer(11000,2000) {
 
-        InputStreamReader streamReader = null;
-        streamReader = new InputStreamReader(input, Charset.forName("UTF-8"));
-        CSVReader reader = new CSVReader(streamReader); // 1. CSVReader 생성
-
-        String[] nextLine;
-
-        int j = 0;
-
-        while ((nextLine = reader.readNext()) != null) {   // 2. CSV 파일을 한줄씩 읽음
-            Log.d("TAG", nextLine[0] + "|" + nextLine[1] + "|"
-                    + nextLine[2] + "|" + nextLine[3] + "\n");
-            dataArray[j] = new CsvData();
-            dataArray[j].number = nextLine[0];
-            dataArray[j].words = nextLine[1];
-            dataArray[j].meaning = nextLine[2];
-            dataArray[j].grade = nextLine[3];
-            j++;
-            if (j == 30)
-                break;
-        }
-    }
-
-    public void countDownTimer() {
-        countDownTimer = new CountDownTimer(11000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-                timerTV.setText(String.valueOf(countTime));
-                timerPB.setProgress(countTime);
-                countTime--;
+                timerTV.setText(String.valueOf(count_timer));
+                timerPB.setProgress(count_timer);
+                count_timer--;
             }
 
             @Override
             public void onFinish() {
+                count_timer = 11;
+                countDownTimer_battle.start();
+                count_word++;
+                questionTV.setText(dataArray[count_word].meaning);
+
+                setBtnLayout.removeAllViews(); // 버튼 레이아웃 초기화
+                answerTV.setText(""); // 정답창 초기화
+                hintTV.setText(""); // 힌트창 초기화
+                countWordLength=0;
+
+                word = dataArray[count_word].words; // CSC 파일에서 단어를 불러옴
+                String splitWord[] = word.split(""); // 단어를 한글자씩 나누어 배열화
+                splitWord = shuffle(splitWord); // 배열을 랜덤으로 섞는다
+
+                for (int i = 0; i < word.length(); i++) { // 랜덤으로 섞을 단어배열을 버튼화한다
+                    hintTV.setText(hintTV.getText() + splitWord[i]+" ");
+
+                    wordBtn[i] = new Button(BattleActivity.this);
+                    wordBtn[i].setText(splitWord[i]);
+                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(130, 130);
+                    params.setMargins(12,10,12,10);
+                    wordBtn[i].setLayoutParams(params);
+                    wordBtn[i].setTextColor(Color.parseColor("#00A3E1"));
+                    wordBtn[i].setTextSize(28);
+                    wordBtn[i].setBackgroundResource(R.drawable.wordbtn);
+
+                    final String getSplitWord = splitWord[i];
+                    final int geti = i;
+
+                    wordBtn[i].setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            answerTV.setText(answerTV.getText()+getSplitWord); // 클릭한 버튼의 텍스트 정답창에 입력
+                            setBtnLayout.removeView(wordBtn[geti]); // 입력한 버튼 삭제
+                            countWordLength++;
+
+                            if(countWordLength == word.length()){
+                                if(word.equals(answerTV.getText())) {
+                                    hintTV.setText("HIT!");
+                                    enemyPB.setProgress(enemyPB.getProgress() - 1);
+                                    count_word++;
+                                } else {
+                                    hintTV.setText("! "+word+" !");
+                                    userPB.setProgress(userPB.getProgress() - 1);
+                                    count_word++;
+                                }
+                            }
+                        }
+                    });
+                    setBtnLayout.addView(wordBtn[i]); // 동적 버튼 추가
+                }
             }
         }.start();
     }
@@ -206,10 +240,12 @@ public class BattleActivity extends AppCompatActivity {
     public void onDestroy() {
         super.onDestroy();
         try {
-            countDownTimer.cancel();
+            countDownTimer_battle.cancel();
+            countDownTimer_onStart.cancel();
         } catch (Exception e) {
         }
-        countDownTimer = null;
+        countDownTimer_battle = null;
+        countDownTimer_onStart=null;
     }
 
     public class CsvData {
